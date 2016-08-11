3c2131696e2adcfe228e8f75e25d2688b035ba90
Aaron Hoffer <aaron.hoffer@nomissolutions.com>
2016-08-10 23:10:25 -0700

    Adding comments and documentation for functions. Updating where the main response ends to comply with all the functions

--- R/NextBus.R
+++ R/NextBus.R
@@ -1,8 +1,23 @@
-#' NextBus: Wrapper for the NextBus API
+#' rnextbus: Wrapper for the NextBus API
 #'
 #' This package is a connection to the NextBus API.
 #' @section NextBus functions:
 #' The NextBus functions
+#'nb_list_agencies
+#'nb_list_routes
+#'nb_route_config
+#'nb_schedule
+#'nb_messages
+#'nb_vehicle_locations
+#' @sections Data limitations
+#' From the current NextBus API PDF, the current limitations exist:
+#' Maximum characters per requester for all commands (IP address): 2MB/20sec
+#' Maximum routes per "routeConfig" command: 100
+#' Maximum stops per route for the "predictionsForMultiStops" command: 150
+#' Maximum number of predictions per stop for prediction commands: 5
+#' Maximum timespan for "vehicleLocations" command: 5min
+#' @references \url{http://www.nextbus.com/xmlFeedDocs/NextBusXMLFeed.pdf}
+#' @references \url{https://gist.github.com/grantland/7cf4097dd9cdf0dfed14}
 #' @docType package
-#' @name NextBus
+#' @name rnextbus
 NULL
--- R/NextBusAPI.R
+++ R/NextBusAPI.R
@@ -1,21 +1,36 @@
 #' NextBus API
+#'
+#' Base URL function
+#'
 #'@param base_url The base url for the NextBus webservice
 #'@return nextbus_url
-#'@import httr XML data.table
 #'@export
-nextbus_url <- function(base_url = NULL) {
+nextbus_url <- function(base_url = NULL, retry = NULL) {
   if (!missing(base_url)) {
     options(nextbus_url = base_url)
   }
   getOption("nextbus_url", "http://webservices.nextbus.com/service/publicXMLFeed")
+
+  # if (!missing(retry)) {
+  #   options(retry = TRUE)
+  # }
 }
 
 #' NextBus API
+#'
+#' Main call to the API
+#'
 #'@param param a list of parameters for the GET query. Built as part of each of the functions.
 #'@return xml_parse
-#'@import httr XML
+#'@importFrom httr GET content
+#'@importFrom XML xmlParse xmlRoot xmlChildren xmlValue xmlSApply xmlAttrs
 #'@export
 nextbus_api <- function(param) {
+
+  # can use gzip xml for improved speed
+  #Accept-Encoding: gzip, deflate
+
+
   response = httr::GET(nextbus_url(), query = param)
   if (response$status_code != 200) {
     stop(httr::content(response))
@@ -25,84 +40,141 @@ nextbus_api <- function(param) {
     xml_parse = XML::xmlRoot(xml_parse)
     if (names(XML::xmlChildren(xml_parse)) %in% "Error") {
       e = XML::xmlChildren(xml_parse)
-      stop(xmlValue(e$Error[[1]]))
+
+      # if  shouldRetry="true" you can try again because
+      # it could just be a temp server issue. Check the retry option
+
+      # otherwise stop
+
+      stop(XML::xmlValue(e$Error[[1]]))
     } else {
-      return(XML::xmlSApply(xml_parse, XML::xmlAttrs))
+      return(xml_parse)
     }
   }
 }
 
 #'Agency List
+#'
+#'List of agencies in Nextbus
+#'
 #'@return agency_df
-#'@import data.table
+#'@importFrom data.table rbindlist
 #'@export
-list_agencies <- function() {
+nb_list_agencies <- function() {
   cmd = "agencyList"
   response = nextbus_api(list(command = cmd))
+  response = XML::xmlSApply(response, XML::xmlAttrs)
   agency_list = lapply(response, as.list)
   agency_df = data.table::rbindlist(agency_list, fill = TRUE)
   return(agency_df)
 }
 
-#' Route list
+#'Route list
+#'
+#'List of routes for a  given bus agency
+#'
 #'@param agency The tag name of the bus agency. Can get from \code{list_agencies()} function
 #'@return route_list
 #'@export
-list_routes <- function(agency) {
+nb_list_routes <- function(agency) {
   cmd = "routeList"
   response = nextbus_api(list(command = cmd, a = agency))
+  response = XML::xmlSApply(response, XML::xmlAttrs)
   route_list = lapply(response, as.list)
+  route_list = unlist(route_list)
   return(route_list)
 }
 
 #'Route Configuration
 #'
+#'Configuration of a particular route for an agency
+#'
 #'@param agency The tag name of the bus agency. Can get from \code{list_agencies()} function
 #'@param route The name of the route. Can get from \code{route_list()} function
-#'@return route_config
+#'@param path Set to TRUE if you want to include path information, this can slow down the query because it's a lot of data. default = FALSE
+#'@return route_config list(route_info, stops, directions, paths)
 #'@export
-route_config <- function(agency, route) {
+nb_route_config <- function(agency, route, path = FALSE) {
   cmd = "routeConfig"
+  if (path) {
+    "&terse"
+  }
   response = nextbus_api(list(command = cmd, a = agency, r = route))
+
+  route_desc = XML::xmlSApply(response, XML::xmlAttrs)
+
+  response = XML::xmlSApply(response[["route"]], XML::xmlAttrs)
   route_config = lapply(response, as.list)
-  return(route_config)
+  stop_table = data.table::rbindlist(route_config, fill = TRUE)
+
+  # for each direction. not necessarily two directions
+
+  # combine the paths inside a single path, but not across
+
+  return(list(route = route_desc, stops = stop_table, directions = NULL, paths = NULL))
 }
 
-#'Predictions
+#'Bus Predictions
 #'
 #'Combine the two predictions for stops
+#'
 #'@param agency The tag name of the bus agency. Can get from \code{list_agencies()} function
 #'@param route The name of the route. Can get from \code{route_list()} function
 #'@param stop The name of the stop. Can get from \code{route_config()} function.
+#'@param short_titles Use short titles for shortening the names. Can be good for smaller displays.
 #'@return predictions
 #'@export
-predictions <- function(agency, route, stop) {
+nb_predictions <- function(agency, route, stop, short_titles = FALSE) {
 
   if (length(stop) > 1) {
     cmd = "predictionsForMultiStops"
+
+    #need to apply the route and stop in the form stops=N|3031&stops...
+    stops = paste(route, stop, sep = "|")
+    query_params = list(command = cmd, a = agency, r = route)
+    query_params[4:(length(stops) + 3)] = stops
+    names(query_params)  = c("command", "a", "r", rep("stops", length(stops)))
+
   } else {
     cmd = "predictions"
+    query_params = list(command = cmd, a = agency, r = route, s = stop)
   }
-  response = nextbus_api(list(command = cmd, a = agency, r = route, s = stop))
+
+  if (short_titles) {
+    query_params[["useShortTitles"]] = "true"
+  }
+
+  response = nextbus_api(query_params)
+  response = XML::xmlSApply(response, XML::xmlAttrs)
   predictions = lapply(response, as.list)
+
+  # returns potential multiple directions and multiple messages
+
+  # also additional attributes
+
+  # convert epoch to datetime
+
+
   return(predictions)
 }
 
 #'Schedule
 #'
 #'Get the schedule for a particular route
+#'
 #'@param agency The tag name of the bus agency. Can get from \code{list_agencies()} function
 #'@param route The name of the route. Can get from \code{route_list()} function
 #'@return schedule_df
 #'@export
-schedule <- function(agency, route) {
+nb_schedule <- function(agency, route) {
   cmd = "schedule"
   response = nextbus_api(list(command = cmd, a = agency, r = route))
+  response = XML::xmlSApply(response, XML::xmlAttrs)
   schedule = lapply(response, as.list)
   return(schedule)
 }
 
-#'Messages
+#'Agency Messages
 #'
 #'Get the messages for selected routes
 #'
@@ -110,14 +182,15 @@ schedule <- function(agency, route) {
 #'@param route The name of the route. Can get from \code{route_list()} function
 #'@return messages
 #'@export
-messages <- function(agency, route) {
+nb_messages <- function(agency, route) {
   cmd = "messages"
   response = nextbus_api(list(command = cmd, a = agency, r = route))
+  response = XML::xmlSApply(response, XML::xmlAttrs)
   messages = lapply(response, as.list)
   return(messages)
 }
 
-#'Vehicle Location
+#' Vehicle Location
 #'
 #'Get the location of a vehicle
 #'
@@ -126,9 +199,10 @@ messages <- function(agency, route) {
 #'@param time The unix time in msec. Default of t = 0 returns the most recent 15 minutes of data.
 #'@return vehicle_location
 #'@export
-vehicle_locations <- function(agency, route, time = 0) {
+nb_vehicle_locations <- function(agency, route, time = 0) {
   cmd = "vehicleLocations"
   response = nextbus_api(list(command = cmd, a = agency, r = route, t = time))
+  response = XML::xmlSApply(response, XML::xmlAttrs)
   vehicle_locations = lapply(response, as.list)
   return(vehicle_locations)
 }
